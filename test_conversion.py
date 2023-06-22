f9674c6f5400faf8281c84c5e674e43a12f6700b
Noortje Groot <noortje.groot@hu.nl>
2023-06-22 10:32:01 +0200

    added types to test module

--- test_conversion.py
+++ test_conversion.py
@@ -1,15 +1,22 @@
 from conversion import celcius_to_fahrenheit
-from conversion import celcius_to_kelvin
+from conversion import celsius_to_kelvin
+from conversion import check_unit_validity
 
 # Must: start function with "test_" and start filename with "test_" in order to automatically apply pytest
 
 def test_celcius_to_fahrenheit():
     assert celcius_to_fahrenheit(20) == 68
     assert celcius_to_fahrenheit(0) ==32
-    assert celcius_to_fahrenheit(None) is None
+    # assert celcius_to_fahrenheit(None) is None
     
 
 def test_celcius_to_kelvin():
-    assert celcius_to_kelvin(0) == 273.15
-    assert celcius_to_kelvin(None) is None
+    assert celsius_to_kelvin(0) == 273.15
+    # assert celsius_to_kelvin(None) is None
+   
+def test_check_unit_validity():
+    assert check_unit_validity("C") == True
+    assert check_unit_validity("F") == True
+    assert check_unit_validity("K") == True
+    assert check_unit_validity("X") == False
  
