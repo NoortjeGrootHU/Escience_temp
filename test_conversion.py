b46333cd0b157ea6b3e5fe51c6c13809fd18df96
Noortje Groot <noortje.groot@hu.nl>
2023-06-22 10:55:02 +0200

    fix pytest

--- test_conversion.py
+++ test_conversion.py
@@ -1,6 +1,7 @@
+import pytest
 from conversion import celcius_to_fahrenheit
 from conversion import celsius_to_kelvin
-from conversion import check_unit_validity
+from modular_code import check_unit_validity
 
 # Must: start function with "test_" and start filename with "test_" in order to automatically apply pytest
 
@@ -20,3 +21,13 @@ def test_check_unit_validity():
     assert check_unit_validity("K") == True
     assert check_unit_validity("X") == False
  
+def test_celcius_to_f_invalid():
+    with pytest.raises(TypeError):
+        celcius_to_fahrenheit("Invalid")
+
+def test_celcius_to_f_invalid2():
+    with pytest.raises(KeyError):
+        celcius_to_fahrenheit("Invalid")
+
+# def test_c_to_f_none():
+#     assert celcius_to_fahrenheit(None) is None
